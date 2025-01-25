1c42749e17bb9c9b57c8467f728d2914ae3b0042
Mattias <rm_eriksson@hotmail.com>
2025-01-25 16:44:28 +0100

    Add Test project with some tests

---
+++ TollCalculator/TollCalculatorTests/TollCalculatorLibTests.cs
@@ -0,0 +1,94 @@
+using TollCalculatorLib;
+using TollCalculatorLib.Model;
+
+namespace TollCalculatorTests
+{
+    [TestClass]
+    public class TollCalculatorLibTests
+    {
+
+        private readonly IDictionary <DateTime, int> tollFeeWeekDay = new Dictionary<DateTime, int>
+        {
+            {new DateTime(2025, 1, 20, 6, 0, 0), 8},
+            {new DateTime(2025, 1, 20, 6, 30, 0), 13},
+            {new DateTime(2025, 1, 20, 7, 0, 0), 18},
+            {new DateTime(2025, 1, 20, 8, 0, 0), 13},
+            {new DateTime(2025, 1, 20, 8, 30, 0), 8},
+            {new DateTime(2025, 1, 20, 15, 0, 0), 13},
+            {new DateTime(2025, 1, 20, 15, 30, 0), 18},
+            {new DateTime(2025, 1, 20, 17, 0, 0), 13},
+            {new DateTime(2025, 1, 20, 18, 0, 0), 8},
+            {new DateTime(2025, 1, 20, 18, 30, 0), 0}
+        };
+
+        private readonly IDictionary<DateTime, int> tollFeeWeekendDay = new Dictionary<DateTime, int>
+        {
+            {new DateTime(2025, 1, 25, 6, 0, 0), 0},
+            {new DateTime(2025, 1, 25, 6, 30, 0), 0},
+            {new DateTime(2025, 1, 25, 7, 0, 0), 0},
+            {new DateTime(2025, 1, 25, 8, 0, 0), 0},
+            {new DateTime(2025, 1, 25, 8, 30, 0), 0},
+            {new DateTime(2025, 1, 25, 15, 0, 0), 0},
+            {new DateTime(2025, 1, 25, 15, 30, 0), 0},
+            {new DateTime(2025, 1, 25, 17, 0, 0), 0},
+            {new DateTime(2025, 1, 25, 18, 0, 0), 0},
+            {new DateTime(2025, 1, 25, 18, 30, 0), 0}
+        };
+
+        private readonly Car _car;
+
+        public TollCalculatorLibTests()
+        {
+            _car = new Car();
+        }
+
+
+        [TestMethod]
+        public void Test_GetTollFee_Array_SinglePassageAtDifferentTimes_WithCar_OnWeekDay()
+        {
+            TollCalculator tollCalculator = new TollCalculator();
+            foreach(var tollFee in tollFeeWeekDay)
+            {
+                var fee = tollCalculator.GetTollFee(_car, [tollFee.Key]);
+                Assert.AreEqual(tollFee.Value, fee);
+            }
+        }
+
+        [TestMethod]
+        public void Test_GetTollFee_SingleDateTime_SinglePassageAtDifferentTimes_WithCar_OnWeekDay()
+        { 
+            TollCalculator tollCalculator = new TollCalculator();
+            foreach(var tollFee in tollFeeWeekDay)
+            {
+                var fee = tollCalculator.GetTollFee(tollFee.Key, _car);
+                Assert.AreEqual(tollFee.Value, fee);
+            }
+        }
+
+
+        [TestMethod]
+        public void Test_GetTollFee_Array_SinglePassageAtDifferentTimes_WithCar_OnWeekendDay()
+        {
+            TollCalculator tollCalculator = new TollCalculator();
+            foreach (var tollFee in tollFeeWeekendDay)
+            {
+                var fee = tollCalculator.GetTollFee(_car, [tollFee.Key]);
+                Assert.AreEqual(tollFee.Value, fee);
+            }
+        }
+
+        [TestMethod]
+        public void Test_GetTollFee_SingleDateTime_SinglePassageAtDifferentTimes_WithCar_OnWeekendDay()
+        {
+            TollCalculator tollCalculator = new TollCalculator();
+            foreach (var tollFee in tollFeeWeekendDay)
+            {
+                var fee = tollCalculator.GetTollFee(tollFee.Key, _car);
+                Assert.AreEqual(tollFee.Value, fee);
+            }
+        }
+
+
+
+    }
+}
